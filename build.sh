c686bfe07d62a2cd95f06c10acec271be2656ca1
Andreas Mantzoutas <anmantzoutas@gmail.com>
2022-03-21 14:12:51 +0200

    Update building script

--- build.sh
+++ build.sh
@@ -1,30 +1,28 @@
 #!/bin/sh
-
 echo "Starting environmental variable initialization.."
 sleep 0.5
 
-echo -ne "\e[1A\e[KPostgres Root User Name: "
-read pguser
+echo -ne "\e[1A\e[KPostgres Root User Name: admin"
+sleep 0.3
+echo
 echo -ne "\e[1A\e[KPostgres User Password: "
 read -s pgpassword
 echo
 
-echo -ne "\e[1A\e[KMinio Root User Name: "
-read mnuser
+echo -ne "\e[1A\e[KMinio Root User Name: admin"
+sleep 0.3
+echo
 echo -ne "\e[1A\e[KMinio Root User Password: "
 read -s mnpassword
 echo
-
 
 echo -ne "\e[1A\e[KJWT Secret Key: "
 read -s jwtpassword
 echo -e "\e[1A\e[K"
 echo
 
-kubectl create secret generic sensitive-data --from-literal PGUSER=$pguser --from-literal PGPASSWORD=$pgpassword --from-literal MNUSER=$mnuser --from-literal MNPASSWORD=$mnpassword --from-literal JWTPASSWORD=$jwtpassword >/dev/null
-kubectl -n fission-function create secret generic sensitive-data --from-literal PGUSER=$pguser --from-literal PGPASSWORD=$pgpassword --from-literal MNUSER=$mnuser --from-literal MNPASSWORD=$mnpassword --from-literal JWTPASSWORD=$jwtpassword >/dev/null
-
-
+kubectl create secret generic sensitive-data --from-literal PGUSER=admin --from-literal PGPASSWORD=$pgpassword --from-literal MNUSER=admin --from-literal MNPASSWORD=$mnpassword >/dev/null
+kubectl -n fission-function create secret generic sensitive-data --from-literal PGUSER=admin --from-literal PGPASSWORD=$pgpassword --from-literal MNUSER=admin --from-literal MNPASSWORD=$mnpassword --from-literal JWTPASSWORD=$jwtpassword >/dev/null
 
 # Apply cluster configurations
 [ -d "k8s" ] || ( echo "Please run in project root directory" && exit 1 ) 
@@ -43,15 +41,14 @@ zip -jr api-source.zip api/. >/dev/null
 echo -e "\e[1A\e[KBuilding...."
 fission spec apply 1>/dev/null
 
-
 echo -e "\e[1A\e[KRemoving uneeded files..."
 
 # Cleanup
 rm -rf frontend-source.zip api-source.zip app/node_modules app/.next
 
-
 echo -ne "\e[1A\e[K"
 echo "[+] Done"
-x=$(minikube -p fis ip)
+x=$(minikube ip)
+echo "Be sure to enable ingress in minikube cluster, if you haven't already, with: 'minikube addons enable ingress'"
 echo [*] Frontend at: http://$x/
 echo [*] Minio console at: http://$x:31111 
